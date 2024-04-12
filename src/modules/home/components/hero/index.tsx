95636f0e8cc31e2ddd67b08a89712f37c72713e9
zhengyang.wu <rarm18@gmail.com>
2024-04-12 11:35:39 -0700

    change ui

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -3,7 +3,7 @@ import { Button, Heading } from "@medusajs/ui"
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+      <div className="bg-[url('https://wuzystore.s3.amazonaws.com/banner-bg.jpg')] w-full bg-center bg-no-repeat bg-cover absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
--- src/modules/layout/components/side-menu/index.tsx
+++ src/modules/layout/components/side-menu/index.tsx
@@ -42,8 +42,8 @@ const SideMenu = ({ regions }: { regions: Region[] | null }) => {
                 leaveFrom="opacity-100 backdrop-blur-2xl"
                 leaveTo="opacity-0"
               >
-                <Popover.Panel className="flex flex-col absolute w-full pr-4 sm:pr-0 sm:w-1/3 2xl:w-1/4 sm:min-w-min h-[calc(100vh-1rem)] z-30 inset-x-0 text-sm text-ui-fg-on-color m-2 backdrop-blur-2xl">
-                  <div data-testid="nav-menu-popup" className="flex flex-col h-full bg-[rgba(3,7,18,0.5)] rounded-rounded justify-between p-6">
+                <Popover.Panel className="flex flex-col absolute w-full pr-4 sm:pr-0 sm:w-1/3 2xl:w-1/4 sm:min-w-min h-[calc(100vh-1rem)] z-30 inset-x-0 text-sm text-gray-800 m-2 backdrop-blur-2xl">
+                  <div data-testid="nav-menu-popup" className="flex flex-col h-full rounded-rounded bg-[url('https://wuzystore.s3.amazonaws.com/chairs-rug.avif')] bg-center bg-no-repeat bg-cover justify-between p-6">
                     <div className="flex justify-end" id="xmark">
                       <button data-testid="close-menu-button" onClick={close}>
                         <XMark />
