8b970a5273de27bd78f77448ceeb048993fac3bf
zhengyang.wu <rarm18@gmail.com>
2024-04-12 12:18:33 -0700

    add speed insight and Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from "next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 import "styles/globals.css"
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
@@ -12,6 +14,8 @@ export default function RootLayout(props: { children: React.ReactNode }) {
     <html lang="en" data-mode="light">
       <body>
         <main className="relative">{props.children}</main>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
